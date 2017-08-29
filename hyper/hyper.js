dfd91d5ad082643b379351cbdbb9e20ee6c862dc
Sébastien Balayn <sebastien.balayn@gmail.com>
2017-08-29 17:25:36 +0200

    update

---
+++ hyper/hyper.js
@@ -0,0 +1,14 @@
+// Future versions of Hyper may add additional config options,
+// which will not automatically be merged into this file.
+// See https://hyper.is#cfg for all currently supported options.
+
+module.exports = {
+  config: {
+    fontSize: 14,
+    fontFamily: '"Fantasque Sans Mono", monospace',
+    cursorShape: 'BEAM',
+    cursorBlink: true,
+    copyOnSelect: false
+  },
+  plugins: [],
+};
